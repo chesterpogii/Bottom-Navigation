475a7f60e390ff5d5cacd71984b5c25b05cadde0
Don Chester Jimenez <donchesterjimenez4@gmail.com>
2025-09-18 17:51:24 +0800

    Flutter Bottom Navigation

---
+++ lib/pages/project_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProjectPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WELCOME PROJECT PAGE'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+    );
+  }
+}
